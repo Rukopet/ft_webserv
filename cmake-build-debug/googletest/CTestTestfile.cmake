a515dc2c2a183f4e246be9848f5d8c073ff04b7b
rallersia <mssmile333@gmail.com>
2021-07-16 15:59:09 +0300

    rm cmake-build, add git submodule with googletest, add gitignore cmakebuild

--- cmake-build-debug/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/egums/ft_webserv/googletest
-# Build directory: /Users/egums/ft_webserv/cmake-build-debug/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
