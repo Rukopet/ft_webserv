0d1588361c4268a4e3661d360a870a84928ea1ea
egums <dima110.ru@gmail.com>
2021-07-17 12:34:56 +0300

    minor changes to test server

--- Nginx_test_server/for_test_telnet.sh
+++ Nginx_test_server/for_test_telnet.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 { echo "open www.example.com 80"
-sleep 2 
-echo "GET /index.html HTTP/1.1" 
-echo "Host: www.example.com" 
-echo 
-echo 
+sleep 2
+echo "GET /index.html HTTP/1.1"
+echo "Host: www.example.com"
+echo
+echo
 sleep 2; } | telnet
 
 
