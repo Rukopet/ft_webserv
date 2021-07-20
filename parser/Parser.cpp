bf38bf15ca8a6b953f42fc39e41b27fa583a0c9c
Piper Leonel <pleonel@va-e3.kzn.21-school.ru>
2021-07-20 13:48:22 +0300

    parse config function

--- parser/Parser.cpp
+++ parser/Parser.cpp
@@ -7,17 +7,38 @@
 
 Parser *Parser::instance = new Parser();
 
+std::pair<int, std::string> Parser::parseErrorParam(std::ifstream &file) {
+	std::pair<int, std::string> errorPage;
+	std::string inData;
+
+	if (!(file >> errorPage.first) || !(file >> errorPage.second)) {
+		std::cerr << "can't parse error_page" << std::endl;
+		exit(-1);
+	}
+	return (errorPage);
+}
+
+int Parser::parseClientMaxBodySize(std::ifstream &file) {
+	int clientMaxBodySize;
+
+	if (!(file >> clientMaxBodySize)) {
+		std::cerr << "can't parse client_max_body_size" << std::endl;
+		exit(-1);
+	}
+	return (clientMaxBodySize);
+}
+
 ServerConfig Parser::parseServer(std::ifstream &file) {
 	std::map<int, std::string> errorPages;
 	int clientMaxBodySize = 1;
 	std::map<std::string, RouteConfig> routeConfigs;
-	int port = 80;
-	std::string host;
+	int port = -1;
+	std::string host = "";
 	std::set<std::string> serverNames;
 
 	std::string inData;
 	file >> inData;
-	if (inData != "server") {
+	if (inData != "server" || file.eof()) {
 		std::cout << "Wrong data outside server blocks" << std::endl;
 		exit(-1);
 	} else {
@@ -26,7 +47,22 @@ ServerConfig Parser::parseServer(std::ifstream &file) {
 			std::cout << "{ excepted" << std::endl;
 			exit(-1);
 		}
-		//TODO: parse params while inData != "}", if EOF or haven't host/port - exit(-1)
+		while (!(file >> inData) && inData != "}") {
+			if (inData == "error_page") {
+				std::pair<int, std::string> errorPage = parseErrorParam(file);
+				errorPages[errorPage.first] = errorPage.second;
+			} else if (inData == "client_max_body_size") {
+
+			}
+			//TODO: parse params
+		}
+	}
+	if (port == -1 || host == "") {
+		std::cout << "haven't port or host for server config" << std::endl;
+		exit(-1);
+	} else if (file.eof()) {
+		std::cout << "excepted } for server block" << std::endl;
+		exit(-1);
 	}
 	return ServerConfig(errorPages, clientMaxBodySize, routeConfigs, port, host, serverNames);
 }
--- parser/Parser.hpp
+++ parser/Parser.hpp
@@ -7,15 +7,18 @@
 
 #include "IParser.hpp"
 
-class Parser : IParser {
+class Parser : public IParser {
 private:
 	Parser();
 	static Parser* instance;
+	static ServerConfig parseServer(std::ifstream &file);
+	static std::pair<int, std::string> parseErrorParam(std::ifstream &file);
 public:
 	static Parser &getInstance();
+
 	Config parseConfig(std::string filename) override;
 
-	ServerConfig parseServer(std::ifstream &file);
+	static int parseClientMaxBodySize(std::ifstream &file);
 };
 
 #endif //FT_WEBSERVER_PARSER_HPP
